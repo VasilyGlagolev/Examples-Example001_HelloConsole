30ba4b1e8103053e714b083283393d9a224d0c38
VasilyGlagolev <139496040+VasilyGlagolev@users.noreply.github.com>
2023-07-26 22:05:31 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,4 +1,27 @@
-﻿Console.Write("Введите ваше имя ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите первое число: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        int b = int.Parse(Console.ReadLine());
+
+        if (a > b)
+        {
+            Console.WriteLine($"Максимальное число: {a}");
+            Console.WriteLine($"Минимальное число: {b}");
+        }
+        else
+        {
+            Console.WriteLine($"Максимальное число: {b}");
+            Console.WriteLine($"Минимальное число: {a}");
+        }
+
+        Console.ReadKey();
+    }
+}
+
